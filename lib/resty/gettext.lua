e759efa01c0c3d36e9c0beec8a8a8572048caf1a
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-02-17 14:16:21 +0200

    Removed some spaces.

--- lib/resty/gettext.lua
+++ lib/resty/gettext.lua
@@ -8,15 +8,15 @@ local band     = bit.band
 local pcall    = pcall
 ffi_cdef[[
 int libintl_version;
-char *gettext (const char *__msgid);
-char *dgettext (const char *__domainname, const char *__msgid);
-char *dcgettext (const char *__domainname, const char *__msgid, int __category);
-char *ngettext (const char *__msgid1, const char *__msgid2, unsigned long int __n);
-char *dngettext (const char *__domainname, const char *__msgid1, const char *__msgid2, unsigned long int __n);
-char *dcngettext (const char *__domainname, const char *__msgid1, const char *__msgid2, unsigned long int __n, int __category);
-char *textdomain (const char *__domainname);
-char *bindtextdomain (const char *__domainname, const char *__dirname);
-char *bind_textdomain_codeset (const char *__domainname, const char *__codeset);
+char *gettext(const char *__msgid);
+char *dgettext(const char *__domainname, const char *__msgid);
+char *dcgettext(const char *__domainname, const char *__msgid, int __category);
+char *ngettext(const char *__msgid1, const char *__msgid2, unsigned long int __n);
+char *dngettext(const char *__domainname, const char *__msgid1, const char *__msgid2, unsigned long int __n);
+char *dcngettext(const char *__domainname, const char *__msgid1, const char *__msgid2, unsigned long int __n, int __category);
+char *textdomain(const char *__domainname);
+char *bindtextdomain(const char *__domainname, const char *__dirname);
+char *bind_textdomain_codeset(const char *__domainname, const char *__codeset);
 ]]
 local ok, lib = pcall(ffi_load, "gettextlib")
 if not ok then
